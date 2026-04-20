ac2838d521b22395604cf81e9f6c938a50178b98
Pavan Kumar Roy <pavansa718@gmail.com>
2026-04-20 17:35:12 +0530

    Added state management with ngrx signal store to handle the wish list and all. Also, implemented pop-ups with help of Toster as a service. Hence, done with the dependency injection.

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,15 +1,26 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, inject, input, output } from '@angular/core';
 import { Product } from '../../models/products';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { ECommerceStore } from '../../e-commerce-store';
 
 @Component({
   selector: 'app-product-card',
-  imports: [MatButton, MatIcon, CommonModule],
+  imports: [MatButton, MatIcon, CommonModule, MatIconButton],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
 })
 export class ProductCard {
   product = input.required<Product>();
+  addToCartClicked = output<Product>();
+  store = inject(ECommerceStore);
+  isInWishlist = computed(() => this.store.wishlistItems().find(p => p.id === this.product().id));
+  toggleWishlist(product: Product) {
+    if(this.isInWishlist()) {
+      this.store.removeFromWishlist(product);
+    } else {
+      this.store.addToWishlist(product);
+    }
+  }
 }
